e977c1316dd4892bc78cf554dab8c25609d74ce4
OlivierLESAGE <contact@lesageolivier.fr>
2021-01-13 09:21:19 +0100

    fix tests

--- sum_test.py
+++ sum_test.py
@@ -4,9 +4,9 @@ from sum import mysum
 @pytest.mark.parametrize('numbers, output', [
     ([1, 2, 3], 6),
     ([1, 2, 3, 4, 5], 15),
-    ([1, 2, 3, 4], 10),
-    ([12,12], 24),
-    ([1, 2, 3, 21], 27)
+    ([1, 2, 3, 3, 1], 10),
+    ([12,11, 1], 24),
+    ([1, 2, 3, 1,20], 27)
     # Ajoutez des tests pour que la somme des éléments de la liste soit égale à 10, 24 et 27
 ])
 def test_mysum(numbers, output):
